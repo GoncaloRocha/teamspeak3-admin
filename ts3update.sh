0d3392bdb2b2ea875effddf3f2a04a02b39934b7
Gonçalo Rocha <goncalo.stone@gmail.com>
2015-08-05 16:10:50 +0100

    Update ts3update.sh
    added --strip 1. rewrote to have some "&&" in the code. changed su to sudo -u.

--- ts3update.sh
+++ ts3update.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-
-TS3=/home/teamspeak/teamspeak
-STARTSCRIPT=ts3server_startscript.sh
+DIR=/home/teamspeak/teamspeak/
+TS3=ts3server_startscript.sh
 USER=teamspeak
 NOW=$(date "+%Y-%m-%d-%H%M%S")
 
@@ -12,10 +11,6 @@ if [ $# -ne 1 ] || [ $EUID != 0 ]; then
 fi
 
 
-cd $TS3
-su $USER -c "$STARTSCRIPT stop"
-tar --exclude='./backup' -zcvf backup/backup-$NOW.tar.gz .
-wget $1
-tar -zxvf ${1##*/} .
-rm -rf ${1##*/}
-su $USER -c "$STARTSCRIPT start"
+cd $DIR && sudo -u $USER ./$TS3 stop
+tar --exclude='./backup' -zcvf backup/backup-$NOW.tar.gz . && wget $1 && tar -zxvf ${1##*/} --strip 1 && rm -rf ${1##*/}
+sudo -u $USER ./$TS3 start
